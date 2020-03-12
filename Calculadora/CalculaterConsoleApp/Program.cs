df4efc4c64fe6c55e14ae2d16508f03014a16fac
cmevrgarcia <c.garcia@recodme.pt>
2020-03-12 17:49:40 +0000

    Added gitignore and console aplication

---
+++ Calculadora/CalculaterConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculaterConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
